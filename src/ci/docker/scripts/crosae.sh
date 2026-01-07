0d680f9015c09a022813b5973bf53787928cc695
Ben Wu <soggysocks206@gmail.com>
2026-01-07 09:35:22 -0800

    Make sure stage 0 RUSTC is set for stage 0 cargo

--- src/ci/docker/scripts/crosae.sh
+++ src/ci/docker/scripts/crosae.sh
@@ -4,5 +4,4 @@ DISABLE_INSTRUMENTATION=1 ../x --stage 2 build std
 
 cd ../daikon_tests
 
-# cargo is in build/host/stage0/bin/
-/checkout/obj/build/host/stage0/bin/cargo run
+RUSTC=/checkout/obj/build/host/stage0/bin/rustc /checkout/obj/build/host/stage0/bin/cargo run
