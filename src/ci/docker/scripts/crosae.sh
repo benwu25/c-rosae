20dbee2f075c2bf9c6c63ef0aaad97cbfc2addef
Ben Wu <soggysocks206@gmail.com>
2026-01-07 00:25:23 -0800

    revert build command

--- src/ci/docker/scripts/crosae.sh
+++ src/ci/docker/scripts/crosae.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DISABLE_INSTRUMENTATION=1 ../x --stage 2 --keep-stage 0 --keep-stage-std 0 --keep-stage 1 --keep-stage-std 1 build std
+DISABLE_INSTRUMENTATION=1 ../x --stage 2 build std
 
 cd ../daikon_tests
 
