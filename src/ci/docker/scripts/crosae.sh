6136e180287ca6ed8e14122ebcd30788074e779a
Ben Wu <soggysocks206@gmail.com>
2026-01-07 11:33:12 -0800

    use --target-dir to avoid read-only file system

--- src/ci/docker/scripts/crosae.sh
+++ src/ci/docker/scripts/crosae.sh
@@ -4,4 +4,4 @@ DISABLE_INSTRUMENTATION=1 ../x --stage 2 build std
 
 cd ../daikon_tests
 
-RUSTC=/checkout/obj/build/host/stage0/bin/rustc /checkout/obj/build/host/stage0/bin/cargo run
+RUSTC=/checkout/obj/build/host/stage0/bin/rustc /checkout/obj/build/host/stage0/bin/cargo run --target-dir /checkout/obj/target_daikon_tests
