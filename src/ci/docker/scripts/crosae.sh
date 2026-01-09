536029a3ad2cc1492d2f429203ca655ff85eabdb
Ben Wu <soggysocks206@gmail.com>
2026-01-09 09:52:37 -0800

    add some fixes

--- src/ci/docker/scripts/crosae.sh
+++ src/ci/docker/scripts/crosae.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Build a stage 2 c-rosae compiler and run the c-rosae tests with the
+# stage2 build
+
 DISABLE_INSTRUMENTATION=1 ../x --stage 2 build std
 
 # move the testing environment to /checkout/obj since /checkout is read-only
@@ -10,4 +13,6 @@ cp -r /checkout/daikon_tests/test /checkout/obj/daikon_tests/
 cd /checkout/daikon_tests
 
 # the tests will run in the new environment with the CI environment variable set
-CROSAE_CI=1 RUSTC=/checkout/obj/build/host/stage0/bin/rustc /checkout/obj/build/host/stage0/bin/cargo run --target-dir /checkout/obj/target_daikon_tests
+CROSAE_CI=1 RUSTC=/checkout/obj/build/host/stage0/bin/rustc \
+/checkout/obj/build/host/stage0/bin/cargo run --target-dir  \
+/checkout/obj/target_daikon_tests
